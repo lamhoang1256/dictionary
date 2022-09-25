e24aa5920f933e06c9fb009b193be27e6ba5ce7c
lamhoang1256 <lamhoang1256@gmail.com>
2022-09-25 10:43:42 +0700

    Feat: api add search word by text

--- pages/api/word/[id].ts
+++ pages/api/word/[id].ts
@@ -1,16 +1,18 @@
 import { STATUS } from "constants/status";
 import words from "data/words.json";
 import { NextApiRequest, NextApiResponse } from "next";
+import { IWord } from "types";
 import catchAsync from "utils/catch-async";
 import { saveWords } from "utils/helperApi";
 import { ApiError, responseError, responseSuccess } from "utils/response";
 
-const wordApi = async (req: NextApiRequest, res: NextApiResponse) => {
+const wordApiWithId = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method, query } = req;
   const { id } = query;
+  let cloneWords: any = [...words];
   switch (method) {
     case "GET": {
-      const word = words.find((word) => word.id === id);
+      const word = cloneWords.find((word: IWord) => word.id === id);
       if (!word) {
         const error = new ApiError(STATUS.NOT_FOUND, "Không tìm thấy từ vựng");
         responseError(error, res);
@@ -21,43 +23,50 @@ const wordApi = async (req: NextApiRequest, res: NextApiResponse) => {
         data: word,
       };
       responseSuccess(res, response);
+      break;
     }
+
     case "POST": {
-      words.push(req.body);
-      saveWords(words);
+      cloneWords.push(req.body);
+      saveWords(cloneWords);
       const response = {
         message: "Thêm từ vựng thành công!",
       };
       responseSuccess(res, response);
+      break;
     }
+
     case "PUT": {
-      const word = words.find((x) => x.id === id);
+      const word = cloneWords.find((word: IWord) => word.id === id);
       if (!word) {
         const error = new ApiError(STATUS.NOT_FOUND, "Không tìm thấy từ vựng");
         responseError(error, res);
         return;
       }
       Object.assign(word, req.body);
-      saveWords(words);
+      saveWords(cloneWords);
       const response = {
         message: "Cập nhật thông tin từ vựng thành công!",
       };
       responseSuccess(res, response);
+      break;
     }
+
     case "DELETE": {
-      const newWords = words.filter((x) => x.id !== id);
-      saveWords(newWords);
+      cloneWords = cloneWords.filter((word: IWord) => word.id !== id);
+      saveWords(cloneWords);
       const response = {
         message: "Xóa từ vựng thành công!",
       };
       responseSuccess(res, response);
+      break;
     }
+
     default: {
-      res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
-      const error = new ApiError(STATUS.METHOD_NOT_ALLOWED, "Method ${method} Not Allowed");
+      const error = new ApiError(STATUS.METHOD_NOT_ALLOWED, `Method ${method} Not Allowed`);
       responseError(error, res);
     }
   }
 };
 
-export default catchAsync(wordApi);
+export default catchAsync(wordApiWithId);
--- pages/api/word/index.ts
+++ pages/api/word/index.ts
@@ -1,51 +1,43 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { ApiError, responseError, responseSuccess } from "utils/response";
-import fs from "fs";
-import words from "data/words.json";
-import { IWord } from "types";
 import { STATUS } from "constants/status";
+import words from "data/words.json";
+import { NextApiRequest, NextApiResponse } from "next";
+import { IWord } from "types";
 import catchAsync from "utils/catch-async";
-
-const addNewWord = (word: IWord) => {
-  words.push(word);
-  saveData(words);
-};
-
-const updateWord = (id: string, payload: Partial<IWord>) => {
-  const word = words.find((x) => x.id === id);
-  if (!word) return;
-  Object.assign(word, payload);
-  saveData(words);
-};
-
-const deleteWord = (id: string) => {
-  const newWords = words.filter((x) => x.id !== id);
-  saveData(newWords);
-};
-
-const saveData = (newWords: IWord[]) => {
-  fs.writeFileSync("data/words.json", JSON.stringify(newWords, null, 4));
-};
-
-export const wordsApi = {
-  getAll: () => words,
-  getById: (id: string) => words.find((word) => word.id === id),
-  addNewWord,
-  deleteWord,
-  updateWord,
-};
+import { saveWords } from "utils/helperApi";
+import { ApiError, responseError, responseSuccess } from "utils/response";
 
 const wordApi = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req;
-  if (method !== "GET") {
-    const error = new ApiError(STATUS.METHOD_NOT_ALLOWED, "Method not allowed");
-    return responseError(error, res);
+  const { method, query } = req;
+  const search = query.search as string;
+  let cloneWords: any = [...words];
+  switch (method) {
+    case "GET": {
+      if (search) {
+        cloneWords = cloneWords.filter((obj: IWord) => obj.word?.includes(search.toLowerCase()));
+      }
+      const response = {
+        message: "Lấy danh sách từ vựng thành công!",
+        data: cloneWords,
+      };
+      responseSuccess(res, response);
+      break;
+    }
+
+    case "POST": {
+      cloneWords.push(req.body);
+      saveWords(cloneWords);
+      const response = {
+        message: "Thêm từ vựng thành công!",
+      };
+      responseSuccess(res, response);
+      break;
+    }
+
+    default: {
+      const error = new ApiError(STATUS.METHOD_NOT_ALLOWED, `Method ${method} Not Allowed`);
+      responseError(error, res);
+    }
   }
-  const response = {
-    message: "Lấy danh sách từ vựng thành công!",
-    data: words,
-  };
-  responseSuccess(res, response);
 };
 
 export default catchAsync(wordApi);
